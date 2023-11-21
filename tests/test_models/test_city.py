dfd4edcb1f3aab90a7d662c0e2f51173f0ce9834
dimka90 <dimkayilrit@gmail.com>
2023-11-21 16:27:01 +0100

    write pydoc for task the test_city file

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,24 +1,34 @@
 #!/usr/bin/python3
-""" """
+"""
+This script contains test cases for the City class.
+"""
 from tests.test_models.test_base_model import test_basemodel
 from models.city import City
 
 
-class test_City(test_basemodel):
-    """ """
+class TestCity(test_basemodel):
+    """
+    TestCity class for testing the City model.
+    """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """
+        Initialize a TestCity object.
+        """
         super().__init__(*args, **kwargs)
         self.name = "City"
         self.value = City
 
     def test_state_id(self):
-        """ """
+        """
+        Test the state_id attribute of the City class.
+        """
         new = self.value()
         self.assertEqual(type(new.state_id), str)
 
     def test_name(self):
-        """ """
+        """
+        Test the name attribute of the City class.
+        """
         new = self.value()
         self.assertEqual(type(new.name), str)
