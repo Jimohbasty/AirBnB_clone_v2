30e03268d5204a8e99de87b16f521c25ca789d31
jimohbasty <jimohabdulbasit@gmail.com>
2023-11-22 10:07:23 +0100

    create

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,34 +1,24 @@
 #!/usr/bin/python3
-"""
-This script contains test cases for the City class.
-"""
+""" """
 from tests.test_models.test_base_model import test_basemodel
 from models.city import City
 
 
-class TestCity(test_basemodel):
-    """
-    TestCity class for testing the City model.
-    """
+class test_City(test_basemodel):
+    """ """
 
     def __init__(self, *args, **kwargs):
-        """
-        Initialize a TestCity object.
-        """
+        """ """
         super().__init__(*args, **kwargs)
         self.name = "City"
         self.value = City
 
     def test_state_id(self):
-        """
-        Test the state_id attribute of the City class.
-        """
+        """ """
         new = self.value()
         self.assertEqual(type(new.state_id), str)
 
     def test_name(self):
-        """
-        Test the name attribute of the City class.
-        """
+        """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
