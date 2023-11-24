38e718f7e4779af105524080b8639e134f2c6d70
jimohbasty <jimohabdulbasit@gmail.com>
2023-11-24 12:00:46 +0100

    task 2

--- models/review.py
+++ models/review.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+""" Place Module for HBNB project """
+from models.base_model import BaseModel
 
 
-class Review(BaseModel, Base):
-    """ Review classto store review information """
-    __tablename__ = 'reviews'
-    text = Column(
-            String(1024),
-            nullable=False)
-    place_id = Column(
-            String(60),
-            ForeignKey('places.id'),
-            nullable=False)
-    user_id = Column(
-            String(60),
-            ForeignKey('users.id'),
-            nullable=False)
+class Place(BaseModel):
+    """ A place to stay """
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
