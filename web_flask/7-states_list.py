9f84d4a6834eb88bdd22af797102fe68399182a4
jimohbasty <jimohabdulbasit@gmail.com>
2024-01-01 21:27:52 +0100

    task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,34 @@
 #!/usr/bin/python3
+"""script that starts a Flask web application
 """
-starts a Flask web application
-"""
-
 from flask import Flask, render_template
-from models import *
 from models import storage
+from models.state import State
+
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """display a HTML page with the states listed in alphabetical order"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+@app.route('/states_list')
+def state_list():
+    """display a HTML page: inside the tag <BODY>
+    <H1> tag: 'States'
+    <UL> tag: 'List of all State objects present in DBStorage
+    <LI> tag: description of one State
+    """
+    states = storage.all(State).values()
+
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def teardown(exceptions):
+    """Remove the current SQLAlchemy Session
+    after each request
+    """
     storage.close()
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5000', debug=True)
